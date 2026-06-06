f2a50a8df5523b88889fee1bce28d2835ead8e99
emilia-27 <B4200930@ulive.pccu.edu.tw>
2026-06-07 04:54:49 +0800

    Add files via upload

---
+++ Tile.h
@@ -0,0 +1,17 @@
+﻿#ifndef Tile_H
+#define Tile_H
+
+#include<string>
+using namespace std;
+
+class Tile {
+public:
+	int suit;  //0:萬,1:條,2:筒,3:自牌
+	int value;//1~9 for m,s,p; 1~7 for honor
+
+	Tile(int s, int v);
+	string toString() const;
+
+};
+
+#endif
