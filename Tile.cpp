b45c5d70dc68482db042ed898e68db045b79838e
emilia-27 <B4200930@ulive.pccu.edu.tw>
2026-06-07 04:55:24 +0800

    Add files via upload

---
+++ Tile.cpp
@@ -0,0 +1,11 @@
+﻿#include"Tile.h"
+
+Tile::Tile(int s, int v) :suit(s), value(v) {}
+
+string Tile::toString() const {
+	if(suit == 0) return to_string(value) + "萬";
+	if(suit == 1) return to_string(value) + "條";
+	if(suit == 2) return to_string(value) + "筒";
+	string honors[] = { "東","南","西","北","白","發","中" };
+	return honors[value];
+}
